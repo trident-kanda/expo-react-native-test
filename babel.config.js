16cb24c18dc50984007093fe7c7ddd5c3f2a42fb
trident-kanda <tridentkandayuki@gmail.com>
2021-11-14 00:21:07 +0900

    remove

--- babel.config.js
+++ babel.config.js
@@ -2,18 +2,7 @@ module.exports = function(api) {
   api.cache(true);
   return {
     plugins: [
-      ["module:react-native-dotenv",{
-        "envName": "APP_ENV",
-        "moduleName": "@env",
-        "path": ".env",
-        "blocklist": null,
-        "allowlist": null,
-        "blacklist": null, // DEPRECATED
-        "whitelist": null, // DEPRECATED
-        "safe": false,
-        "allowUndefined": true,
-        "verbose": false
-      }]
+     
       ],
     presets: ['babel-preset-expo'],
   };
