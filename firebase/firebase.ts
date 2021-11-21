3878968474be6bafd0e3e90235ba6bac37dd5379
trident-kanda <tridentkandayuki@gmail.com>
2021-11-22 00:18:29 +0900

    firestore

--- firebase/firebase.ts
+++ firebase/firebase.ts
@@ -1,7 +1,10 @@
 import { initializeApp } from "firebase/app"
 import { getAuth } from "firebase/auth"
+import { getFirestore} from "firebase/firestore"
 import env from "../env";
+
 const firebaseApp = initializeApp(env.expo.firebaseConfig);
 
 export const auth = getAuth()
+export const db = getFirestore()
 
